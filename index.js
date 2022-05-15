2f9917abf28010400274109c346deed0e33ac426
DanMarqz <danielarturo.marquez@gmail.com>
2022-05-15 16:56:43 -0400

    ADD: Template engine & REST API

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Settings
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// Routes
+app.get('/api/users', (req, res) => {
+  res.json(
+    [
+      {name: 'John'}, 
+      {name: 'Sara'}
+    ]
+  )
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {
+    name: 'John',
+    age: 24
+  })
+});
+
+
+// Static Files
+app.use(express.static('public'));
+
+// Starting Server
 app.listen(port);
 console.log(`Server listening on port ${port}`);
